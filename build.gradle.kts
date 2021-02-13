4f2150438923b396b36bf31716ecbbdb53159ecb
kamko <j@necek.com>
2021-02-13 16:00:04 +0100

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "1.3.71"
-    id("com.google.cloud.tools.jib") version "2.2.0"
-    id("com.gorylenko.gradle-git-properties") version "2.2.2"
+    kotlin("jvm") version "1.4.30"
+    id("com.google.cloud.tools.jib") version "2.7.1"
+    id("com.gorylenko.gradle-git-properties") version "2.2.4"
 }
 
 group = "dev.kamko"
-version = "0.0.1"
+version = "0.0.2"
 
 repositories {
     mavenCentral()
@@ -20,8 +20,8 @@ tasks.withType(KotlinCompile::class.java).configureEach {
 
 jib {
     from {
-        // adoptopenjdk:14.0.1_7-jre-hotspot-bionic
-        image = "adoptopenjdk@sha256:b344e0582051eceaec9d56ca09443184fa5b0d95abf6eb62362393de99ba7710"
+        // adoptopenjdk:14.0.2_12-jre-hotspot-bionic
+        image = "adoptopenjdk@sha256:934333a1462f5f011b07c20e893165ff75f26ad8b782834b601b8c3d2a43bac5"
     }
 
     to {
@@ -40,11 +40,11 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("org.slf4j:slf4j-api:'1.7.30")
 
-    implementation("com.github.pengrad:java-telegram-bot-api:4.8.0")
+    implementation("com.github.pengrad:java-telegram-bot-api:5.0.1")
     implementation("org.quartz-scheduler:quartz:2.3.2")
-    implementation("net.lingala.zip4j:zip4j:2.5.2")
-    implementation("org.xerial:sqlite-jdbc:3.31.1")
+    implementation("net.lingala.zip4j:zip4j:2.6.4")
+    implementation("org.xerial:sqlite-jdbc:3.34.0")
 
-    implementation("com.backblaze.b2:b2-sdk-core:3.1.0")
-    implementation("com.backblaze.b2:b2-sdk-httpclient:3.1.0")
+    implementation("com.backblaze.b2:b2-sdk-core:4.0.0")
+    implementation("com.backblaze.b2:b2-sdk-httpclient:4.0.0")
 }
